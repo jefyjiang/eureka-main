ebb36df96cdd16d62a1902f6505a11cd512768a2
jiang <jefyjiang@hotmail.com>
2018-02-13 13:56:45 +0800

    测试代码中的中文在gitlib页面上出现乱码

--- eureka-server/src/test/java/com/jlearn/sc/eureka/server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/jlearn/sc/eureka/server/EurekaServerApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EurekaServerApplicationTests {
-
+//测试中文显示
 	@Test
 	public void contextLoads() {
 	}
